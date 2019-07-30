2fde6ea33eaca8a4a24f403425f6e420b2356337
Arthur Vickers <ajcvickers@hotmail.com>
2019-07-30 09:52:45 -0700

    Fix SQLite affinity type mapping to correctly find conversions
    Fixes #16111
    
    The issue was that when finding a type mapping based on type name affinity we were not checking that it was compatible with the CLR type. This resulted in short-circuiting of the conversion selection and hence the wrong mapping.

--- test/EFCore.Sqlite.Tests/Storage/SqliteTypeMappingTest.cs
+++ test/EFCore.Sqlite.Tests/Storage/SqliteTypeMappingTest.cs
@@ -2,8 +2,11 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data;
 using System.Data.Common;
+using System.Linq;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore.Sqlite.Storage.Internal;
 using Microsoft.EntityFrameworkCore.TestUtilities;
@@ -14,6 +17,65 @@ namespace Microsoft.EntityFrameworkCore.Storage
 {
     public class SqliteTypeMappingTest : RelationalTypeMappingTest
     {
+        private class YouNoTinyContext : DbContext
+        {
+            private readonly SqliteConnection _connection;
+
+            public YouNoTinyContext(SqliteConnection connection)
+                => _connection = connection;
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+                => optionsBuilder.UseSqlite(_connection);
+
+            public DbSet<NoTiny> NoTinnies { get; set; }
+        }
+
+        private enum TinyState : byte
+        {
+            One,
+            Two,
+            Three
+        }
+
+        private class NoTiny
+        {
+            [Key]
+            public int Id { get; set; }
+
+            [Required]
+            [Column(TypeName = "tinyint")]
+            public TinyState TinyState { get; set; }
+        }
+
+        [ConditionalFact]
+        public void SQLite_type_mapping_works_even_when_using_non_SQLite_store_type()
+        {
+            using (var connection = new SqliteConnection("DataSource=:memory:"))
+            {
+                connection.Open();
+
+                using (var context = new YouNoTinyContext(connection))
+                {
+                    context.Database.EnsureCreated();
+
+                    context.Add(
+                        new NoTiny
+                        {
+                            TinyState = TinyState.Two
+                        });
+                    context.SaveChanges();
+                }
+
+                using (var context = new YouNoTinyContext(connection))
+                {
+                    var tiny = context.NoTinnies.Single();
+                    Assert.Equal(TinyState.Two, tiny.TinyState);
+                }
+
+                connection.Close();
+            }
+        }
+
         protected override DbCommand CreateTestCommand()
             => new SqliteCommand();
 
@@ -34,7 +96,7 @@ namespace Microsoft.EntityFrameworkCore.Storage
         [InlineData("TEXT", typeof(string))]
         [InlineData("Integer", typeof(long))]
         [InlineData("Blob", typeof(byte[]))]
-        [InlineData("numeric", typeof(byte[]))]
+        [InlineData("numeric", null)]
         [InlineData("real", typeof(double))]
         [InlineData("doub", typeof(double))]
         [InlineData("int", typeof(long))]
@@ -42,14 +104,14 @@ namespace Microsoft.EntityFrameworkCore.Storage
         [InlineData("UNSIGNED BIG INT", typeof(long))]
         [InlineData("VARCHAR(255)", typeof(string))]
         [InlineData("nchar(55)", typeof(string))]
-        [InlineData("datetime", typeof(byte[]))]
-        [InlineData("decimal(10,4)", typeof(byte[]))]
-        [InlineData("boolean", typeof(byte[]))]
-        [InlineData("unknown_type", typeof(byte[]))]
-        [InlineData("", typeof(byte[]))]
+        [InlineData("datetime", null)]
+        [InlineData("decimal(10,4)", null)]
+        [InlineData("boolean", null)]
+        [InlineData("unknown_type", null)]
+        [InlineData("", null)]
         public void It_maps_strings_to_not_null_types(string typeName, Type clrType)
         {
-            Assert.Equal(clrType, CreateTypeMapper().FindMapping(typeName).ClrType);
+            Assert.Equal(clrType, CreateTypeMapper().FindMapping(typeName)?.ClrType);
         }
 
         private static IRelationalTypeMappingSource CreateTypeMapper()
